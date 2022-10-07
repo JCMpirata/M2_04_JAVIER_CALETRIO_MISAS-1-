97059154db1af78d34d3d8119d914eebe7748a43
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-10-07 17:49:50 +0000

    Lista terminada

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+#EJERCICIO 1
+#LISTA
+paises = ["España", "Alemania", "Italia", "Suiza", "Francia", "Inglaterra"]
+print(paises)
+#Segundo elemento
+print(f"El segundo elemento es: ", paises[1])
+#Longitud de la lista
+print(f"La lista mide: ", len(paises))
+#Busqueda en la lista
+print(f"Queremos buscar: ", paises[3])
+#Añadir elemento
+paises.insert(3, "Austria")
+print(paises)
+#Eliminar elemento
+paises.remove("Inglaterra")
+print(paises)
+
 
